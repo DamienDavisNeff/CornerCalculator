525f6fb56f0a1101cb1afe1ab3b502b49921aa2e
Thirdplace Studios <thirdplace.studios.dev@gmail.com>
2021-11-25 11:07:12 -0500

    made more efficient by removing unnecessary code

--- CornerCalculator.cs
+++ CornerCalculator.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
@@ -9,65 +9,47 @@ namespace CornerCalculator {
 
 		public static void Main() {
 		  
-		  Console.WriteLine("Insert X Coordinate");
-		  float centerX = float.Parse(Console.ReadLine());
-		  Console.WriteLine("Insert Z Coordinate");
-		  float centerZ = float.Parse(Console.ReadLine());
+		Console.WriteLine("Insert X Coordinate");
+		float centerX = float.Parse(Console.ReadLine());
+		Console.WriteLine("Insert Z Coordinate");
+		float centerZ = float.Parse(Console.ReadLine());
 
-		  float centerEdgeA;
-		  float centerEdgeB;
-		  float centerEdgeC;
-		  float centerEdgeD;
-		  
-		  float cornerA1;
-		  float cornerA2;
-		  string cornerA;
-		  float cornerB1;
-		  float cornerB2;
-		  string cornerB;
-		  float cornerC1;
-		  float cornerC2;
-		  string cornerC;
-		  float cornerD1;
-		  float cornerD2;
-		  string cornerD;
+		Console.WriteLine("Insert Radius");
+		float radius = float.Parse(Console.ReadLine());
 
-			Console.WriteLine("Insert Radius");
-			float radius = float.Parse(Console.ReadLine());
+		float centerEdgeA = centerX + radius;
+		float centerEdgeB = centerZ + radius;
+		float centerEdgeC = centerX - radius;
+		float centerEdgeD = centerZ - radius;
+			
+		float cornerA1 = centerX + radius;
+		float cornerA2 = centerZ + radius;
+		string cornerA = cornerA1+","+cornerA2;
+			
+		float cornerB1 = centerX - radius;
+		float cornerB2 = centerZ - radius;
+		string cornerB = cornerB1+","+cornerB2;
+			
+		float cornerC1 = centerX + radius;
+		float cornerC2 = centerZ - radius;
+		string cornerC = cornerC1+","+cornerC2;
+			
+		float cornerD1 = centerX - radius;
+		float cornerD2 = centerZ + radius;
+		string cornerD = cornerD1+","+cornerD2;
+			
+		Console.WriteLine("CORNER: "+cornerA);
+		Console.WriteLine("CORNER: "+cornerB);
+		Console.WriteLine("CORNER: "+cornerC);
+		Console.WriteLine("CORNER: "+cornerD);
+			
+		// Console.WriteLine(centerEdgeA+","+centerZ);
+		// Console.WriteLine(centerX+","+centerEdgeB);
+		// Console.WriteLine(centerEdgeC+","+centerZ);
+		// Console.WriteLine(centerX+","+centerEdgeD);
 
-			centerEdgeA = centerX + radius;
-			centerEdgeB = centerZ + radius;
-			centerEdgeC = centerX - radius;
-			centerEdgeD = centerZ - radius;
-			
-			cornerA1 = centerX + radius;
-			cornerA2 = centerZ + radius;
-			cornerA = cornerA1+","+cornerA2;
-			
-			cornerB1 = centerX - radius;
-			cornerB2 = centerZ - radius;
-			cornerB = cornerB1+","+cornerB2;
-			
-			cornerC1 = centerX + radius;
-			cornerC2 = centerZ - radius;
-			cornerC = cornerC1+","+cornerC2;
-			
-			cornerD1 = centerX - radius;
-			cornerD2 = centerZ + radius;
-			cornerD = cornerD1+","+cornerD2;
-			
-			Console.WriteLine("CORNER: "+cornerA);
-			Console.WriteLine("CORNER: "+cornerB);
-			Console.WriteLine("CORNER: "+cornerC);
-			Console.WriteLine("CORNER: "+cornerD);
-			
-			// Console.WriteLine(centerEdgeA+","+centerZ);
-			// Console.WriteLine(centerX+","+centerEdgeB);
-			// Console.WriteLine(centerEdgeC+","+centerZ);
-			// Console.WriteLine(centerX+","+centerEdgeD);
-
-            Console.WriteLine("Press 'Enter' To Close");
-            Console.ReadLine();
+        Console.WriteLine("Press 'Enter' To Close");
+        Console.ReadLine();
 
 		}
 	}
